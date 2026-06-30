a32eff72904f550da4526d5750064ce8798f8f2d
Tro0021 <abhirambellamkonda80@gmail.com>
2026-06-30 07:01:56 +0530

    2 hours 40 mins

---
+++ scripts/ore.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var ore_data: OreData
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+func _ready() -> void:
+	sprite_2d.texture = ore_data.texture
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		if body.add_ore(ore_data):
+			queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,25 @@
+class_name Player
 extends CharacterBody2D
 
 const SPEED = 100.0
 
 var is_mining: bool = false
+var hitbox_offset: Vector2
 var last_direction: Vector2 = Vector2.RIGHT
+var detected_rocks: Array = []
+var pickaxe_strength: int = 1
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hit_box: Area2D = $HitBox
+@onready var hit_box_collision_shape_2d: CollisionShape2D = $HitBox/CollisionShape2D
+@onready var mining_timer: Timer = $MiningTimer
+@onready var pickaxe_hit_sound: AudioStreamPlayer2D = $PickaxeHitSound
+
+func _ready() -> void:
+	hitbox_offset = hit_box.position
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("use_pickaxe"):
+	if Input.is_action_pressed("use_pickaxe") and mining_timer.is_stopped():
 		use_pickaxe()
 	
 	if is_mining:
@@ -25,10 +36,13 @@ func process_movement() -> void:
 	if direction != Vector2.ZERO:
 		velocity = direction * SPEED
 		last_direction = direction
+		update_hitbox_position()
 	else:
 		velocity = Vector2.ZERO
 
 func process_animation() -> void:
+	hit_box.monitoring = false
+	
 	if velocity != Vector2.ZERO:
 		play_animation("run", last_direction)
 	else:
@@ -43,10 +57,56 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 	elif dir.y > 0:
 		animated_sprite_2d.play(prefix + "_down")
 
+func update_hitbox_position() -> void:
+	var x := hitbox_offset.x
+	var y := hitbox_offset.y
+	
+	match last_direction:
+		Vector2.LEFT:
+			hit_box.position = Vector2(-x, y)
+			hit_box_collision_shape_2d.rotation_degrees = 0
+		Vector2.RIGHT:
+			hit_box.position = Vector2(x, y)
+			hit_box_collision_shape_2d.rotation_degrees = 0
+		Vector2.UP:
+			hit_box.position = Vector2(y, -x)
+			hit_box_collision_shape_2d.rotation_degrees = 90
+		Vector2.DOWN:
+			hit_box.position = Vector2(-y, x)
+			hit_box_collision_shape_2d.rotation_degrees = 90
+
+
 func use_pickaxe() -> void:
+	detected_rocks.clear()
+	hit_box.monitoring = true
 	is_mining = true
+	mining_timer.start()
 	play_animation("swing_pickaxe", last_direction)
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if is_mining:
 		is_mining = false
+		if detected_rocks.size() > 0:
+			var rock_to_hit = get_most_overlapping_rock()
+			rock_to_hit.take_damage(pickaxe_strength)
+			pickaxe_hit_sound.play()
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	if body is Rock:
+		detected_rocks.append(body)
+
+func get_most_overlapping_rock() -> Rock:
+	var best_rock = detected_rocks[0]
+	var best_dist = hit_box.global_position.distance_to(best_rock.global_position)
+	
+	for rock in detected_rocks:
+		var dist = hit_box.global_position.distance_to(rock.global_position)
+		if dist < best_dist:
+			best_dist = dist
+			best_rock = rock
+	
+	return best_rock
+
+func add_ore(data: OreData) -> bool:
+	print(data)
+	return true
